b65c40706ba72122a7cf0c030269e1389e68c10e
Djoe Pramono <djoepramono@gmail.com>
2017-02-14 00:23:51 +1100

    Add more notes on case class topic

--- src/main/scala/functions.scala
+++ src/main/scala/functions.scala
@@ -1,4 +1,4 @@
-package net.studikode.function
+package net.studikode
 
 object HigherOrderFunction  {
   def main(args: Array[String]): Unit  = {
@@ -8,7 +8,6 @@ object HigherOrderFunction  {
     /**
      * Mapping, performing a function on a data structure
      */
-    println("Mapping")
     var myMap = myList.map(v => addOne(v))
     println(myMap)
 
@@ -16,7 +15,6 @@ object HigherOrderFunction  {
      * Folding, pretty much performing a function
      * The first parameter is pretty much a initial value
      */
-    println("Folding")
     var myFold = myList.fold(0)(addTheseTwo(_,_))
     println(myFold)
   }
--- src/main/scala/fundamentals/case-class.scala
+++ src/main/scala/fundamentals/case-class.scala
@@ -1,18 +1,30 @@
-package studikode.scala.fundamentals
+package net.studikode.scala.fundamentals
 
 /**
  * Case class is pretty much the same as regular classes, except
  * - immutable
  * - can be pattern matched
+ * When not to use? When it contains complex behaviour or stateful computations
  */
+
+/**
+ * abstract class or trait?
+ * abstract classes have constructor and type parameters
+ * a class can inherit from one abstract classes but multiple trait
+ */
+
 abstract class Course(manager: String)
 case class RegularCourse(length: Int, caretaker: String) extends Course(caretaker)
-// case class WorkReady(length: Int) extends Course
+case class WorkReady(length: Int, caretaker: String) extends Course(caretaker)
 // case class FastTrack(length: Int) extends Course
 
 object School {
   def main(args: Array[String]) = {
-      var IT = new RegularCourse(2,"John Doe")
-      println(IT)
+
+    // Note that there is no *new* in case class
+    var IT = RegularCourse(2,"John Doe")
+    var Geography = WorkReady(4, "Mary Jane")
+
+    println(IT)
   }
 }
