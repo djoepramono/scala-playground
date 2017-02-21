e222fa5644f02836d5558a8ebecd6918bde9a744
Joe Pramono <joe.pramono@rea-group.com>
2017-02-21 23:19:19 +1100

    Add fold code

---
+++ src/main/scala/fundamentals/fold.scala
@@ -0,0 +1,53 @@
+package net.studikode.scala.fundamentals
+
+object Fold{
+  def main(args: Array[String]) = {
+
+    val myList = List(1,2,3,4,5)
+
+    /**
+     * This basically says
+     * start with an integer 0
+     * add 0 with the first integer (1) on the right
+     *  then add the result to next integer (2) on the right
+     *  and so on ...
+     * continue this until the last integer
+     */
+    val result = myList.foldLeft(0) {
+      (x,y) => {
+        println(s"${x} + ${y} is ${x+y}")
+        x + y
+      }
+    }
+
+    println(s"So the result of left-folding ${myList} is ${result}")
+
+    /**
+     * `fold` and `foldLeft` are basically almost the same thing ...
+     * almost, because there's no guarantee that `fold` starts from the left
+     * it starts from the order of the elements is added
+     */
+    val myLongerList =  5 :: 6 :: myList
+    val result2 = myLongerList.fold(0) {
+      (x,y) => {
+        println(s"${x} + ${y} is ${x+y}")
+        x + y
+      }
+    }
+
+    println(s"So the result of just-folding ${myLongerList} is ${result2}")
+
+    /**
+     * Things go a little bit differently with `foldRight`
+     * The end result is still the same but the sequence changes
+     */
+    val result3 = myList.foldRight(0) {
+      (x,y) => {
+        println(s"${x} + ${y} is ${x+y}")
+        x + y
+      }
+    }
+
+    println(s"So the result of right-folding ${myList} is ${result3}")
+  }
+}
