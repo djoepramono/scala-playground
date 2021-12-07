3c34e8fa5fb24166842636721f8d5bfe7d369bfb
Djoe Pramono <djoepramono@gmail.com>
2021-12-07 21:46:55 +1100

    add lens demo

--- src/main/scala/playground/optics.scala
+++ src/main/scala/playground/optics.scala
@@ -5,7 +5,18 @@ object Optics {
         
         case class Street ( number: Int, name: String );
         case class Address ( street: Street, city: String );
+        val myAddress = Address(Street(123, "Test Road"), "Realtown")
+        System.out.println(s"Hello my street name is ${myAddress.street.name}")
 
+        // If we want to change the street name 
+        val output1 = myAddress.copy(
+            street = (myAddress.street.copy(
+                name = "Copied Road")
+            )
+        )        
+        System.out.println(s"Using .copy my street name can be changed to ${output1.street.name}")
+
+        // Introducing Lens
         case class Lens[S, A] (
             get: S => A,
             set: (S, A) => S
@@ -18,18 +29,28 @@ object Optics {
         )
         
         // Given an address, get/set a street
-        val AddressStreetLens = Lens[Address, Street] {
+        val addressStreetLens = Lens[Address, Street] (
             get = (a: Address) => a.street,
             set = (a: Address, s: Street ) => a.copy(street = s)
-        }
+        )
+
+        // See how it works
+        // get
+        System.out.println(s"Using Lens.get my street name is ${streetNameLens.get(myAddress.street)}")
+        // set
+        System.out.println(s"Using Lens.set my street name can be changed to ${streetNameLens.set(myAddress.street, "New Test Road").name}")
+
 
         // Lens does compose
         def composeLens[A,B,C](ab: Lens[A,B], bc: Lens[B,C]): Lens[A,C] = {
-            return Len[A,C](
-                get = bc.get(ab.get(_)),
+            return Lens[A,C](
+                get = (a: A) => bc.get(ab.get(a)),
                 set = (a: A, c: C) => ab.set(a, bc.set(ab.get(a), c))
             )
         }
+        
+        val output = composeLens(addressStreetLens, streetNameLens).get(myAddress)
+        System.out.println(s"Using composed Lens my street name is $output");
 
     }
  }
