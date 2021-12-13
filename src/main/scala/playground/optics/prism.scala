a8085079be9ca858ade87286eb5cbd3f828bea4f
Djoe Pramono <djoepramono@gmail.com>
2021-12-14 00:24:24 +1100

    add more stuffs to prism

--- src/main/scala/playground/optics/prism.scala
+++ src/main/scala/playground/optics/prism.scala
@@ -4,22 +4,32 @@ object Prism {
     def main(args: Array[String]): Unit = {
 
         // Prism is like Lens
+        // Except that it works on optional type*
+        // For simplicity, we would just hardcode Option i.e. Option is not in the type param
         case class Prism[S, A] (
             get: S => Option[A],
             set: (S, A) => S
         )
 
-        // First letter of string
-        case class Staff(name: String, directReport: Option[Staff])
+        // Staff and direct report
+        case class Staff(name: String, directReport: Option[String])
 
 
-        val DirectReportPrism =  Prism[Staff, Staff](
-            get = (s: Staff) => Some(s.charAt(0)),
-            set = (s: String, n: String) => s.replace(s, n)
+        val DirectReportPrism =  Prism[Staff, String](
+            get = (s: Staff) => s.directReport,
+            set = (s: Staff, d: String) => s.copy(directReport = Some(d))
         )
 
-        System.out.println(FirstPrism.get("John Doe"))
+        val john = Staff("John", Some("Jack"))
 
-
+        System.out.println(DirectReportPrism.get(john))
+        
+        // Yup it's composable
+        // def composePrism[A,B,C](ab: Prism[A,B], bc: Prism[B,C]): Lens[A,C] = {
+        //     return Lens[A,C](
+        //         get = (a: A) => bc.get(ab.get(a)),
+        //         set = (a: A, c: C) => ab.set(a, bc.set(ab.get(a), c))
+        //     )
+        // }
     }
 }
