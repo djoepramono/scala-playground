cc6c3e8d6cf9d520bb73b04dc95d6d861e772573
Djoe Pramono <djoepramono@gmail.com>
2021-12-14 23:19:57 +1100

    revamp prism again

--- src/main/scala/playground/optics/prism.scala
+++ src/main/scala/playground/optics/prism.scala
@@ -6,6 +6,8 @@ object Prism {
         // Prism is like Lens
         // Except that it works on optional type*
         // For simplicity, we would just hardcode Option i.e. Option is not in the type param
+        // A is a subtype of S
+        // or is this Optional? i.e. another optic. not prism. Nope it is not because the signature of `set`
         case class Prism[S, A] (
             get: S => Option[A],
             set: (S, A) => S
@@ -16,27 +18,48 @@ object Prism {
         // case class Staff(name: String, directReport: Option[String])
 
         // It's all about sum types not product types
+        // Actually no this is also wrong
+        // is it? maybe not
         sealed trait Vehicle
         case class Car(driver: String) extends Vehicle
         case class Plane(pilot: String) extends Vehicle
 
+        // It should be
+        // note that the struct is similar between Car and Plane
+        // nah nvm this is wrong
+        // sealed trait Vehicle
+        // case class Car(controller: String) extends Vehicle
+        // case class Plane(controller: String) extends Vehicle
 
-        val ControllerPrism =  Prism[Vehicle, String](
+
+        val CarPrism =  Prism[Vehicle, Car](
             get = (v: Vehicle) => v match {
-                case Plane(pilot) => Some(pilot)
-                case Car(driver) => Some(driver)
+                case x: Plane => None
+                case x: Car => Some(x)
             },
-            set = (v: Vehicle, s: String) => v match {
-                case x: Plane => x.copy(pilot = s)
-                case x: Car => x.copy(driver = s)
+            set = (v: Vehicle, s: Car) => v match {
+                case x: Plane => v
+                case x: Car => s
             }
         )
 
         val jeep = Car("Jake")
         val jet = Plane("Jess")
 
-        System.out.println(ControllerPrism.get(jeep))
+        System.out.println(s"The jeep controller is ${(CarPrism.get(jeep).getOrElse(Car("unknown"))).driver}") 
+        System.out.println(s"The jet controller is ${(CarPrism.get(jet).getOrElse(Car("unknown"))).driver}")
         
+
+        // Over prism
+        def overPrism[S,A](sa: Prism[S,A], f: A => A, s:S): S = {
+            sa.get(s) match {
+                case Some(value) => sa.set(s, f(value))
+                case None => s
+            }
+        }
+
+        // System.out.println(s"The jeep controller is uppercased ${overPrism(ControllerPrism, (a: String) => a.toUpperCase(), jeep)}")
+
         // Yup it's composable
         // def composePrism[A,B,C](ab: Prism[A,B], bc: Prism[B,C]): Prism[A,C] = {
         //     return Prism[A,C](
